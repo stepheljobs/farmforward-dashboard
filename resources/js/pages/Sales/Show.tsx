d91b724d811d5e63a9fcc7b734d482e2b30126ba
stepheljobs <stephelgeorgemaca@gmail.com>
2025-04-13 16:43:43 +0800

    format the sales

--- resources/js/pages/Sales/Show.tsx
+++ resources/js/pages/Sales/Show.tsx
@@ -45,7 +45,7 @@ export default function Show({ sale }: Props) {
                                     href="/sales"
                                     className="inline-flex items-center px-4 py-2 bg-white border border-gray-800 rounded-md font-semibold text-xs text-gray-800 uppercase tracking-widest hover:bg-gray-100 focus:bg-gray-100 active:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 transition ease-in-out duration-150"
                                 >
-<ArrowLeft className="w-4 h-4 mr-2" />
+                                    <ArrowLeft className="w-4 h-4 mr-2" />
                                     Back to Sales
                                 </Link>
                             </div>
